ad7d63d4499cd6a1d6125244433992af91211ee4
Thomas <thomasplatschorre@gmail.com>
2022-04-03 11:36:27 +0200

    impl display for FAesKey

--- src/upk_decrypter/src/encryption.rs
+++ src/upk_decrypter/src/encryption.rs
@@ -1,6 +1,7 @@
 use aes::Aes256;
 use block_modes::{BlockMode, Ecb, block_padding::ZeroPadding};
 
+use std::fmt::Display;
 use std::io::SeekFrom;
 use std::ops::Add;
 
@@ -10,9 +11,15 @@ use crate::{Result, ParserError};
 const KEY_SIZE: usize = 32;
 
 #[allow(dead_code)]
-#[derive(Debug, Clone, Copy)] // display to hex
+#[derive(Debug, Clone, Copy)]
 pub struct FAesKey {
     pub(crate) key: [u8; KEY_SIZE]
+}
+
+impl Display for FAesKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.to_hex())
+    }
 }
 
 impl FAesKey {
@@ -28,7 +35,18 @@ impl FAesKey {
         "0x".to_owned().add(&hex::encode(&self.key))
     }
 
-    pub fn decrypt<Ar>(&self, archive: &mut Ar, offset: u64, len: usize) -> Result<()> 
+    pub fn as_bytes(&self) -> &[u8] {
+        &self.key
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let result = vec![0u8; self.key.len()];
+        result.copy_from_slice(&self.key);
+
+        result
+    }
+
+    fn decrypt<Ar>(&self, archive: &mut Ar, offset: u64, len: usize) -> Result<()> 
     where Ar: FArchive {
         let cipher = Ecb::<Aes256, ZeroPadding>::new_from_slices(&self.key, Default::default())?;
 
